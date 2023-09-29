14cd73581867e95d862679e8efd963dc1070d6c0
msgalvan1207 <85647232+msgalvan1207@users.noreply.github.com>
2023-09-28 19:08:00 -0500

    Segundo adelanto

---
+++ src/components/Home.js
@@ -0,0 +1,7 @@
+function Home(){
+    return (
+        <h1>Funcion de home</h1>
+    )
+}
+
+export default Home
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,21 +1,25 @@
 import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row"
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import { useState } from "react";
+import Image from "react-bootstrap/Image";
 
 
 function Login() {
-    const [validationState, setValidationState] = useState({emailValidation: false, passwordValidation: false});
-
-    const handleEmail = () => {
-        console.log("lol")
-    };
-
-    const handlePass = () => {
-        console.log("lmao")
-    };
-
     return (
-        <h1>Hola mundo</h1>
-    )
+      <Container>
+        <Row>
+        <Col className='bg-success'>
+          <Image/>
+          <h2 className='text-center text-white'>Encuentra hasta el libro que no estabas buscando</h2>
+        </Col>
+        <Col>
+          <h1 className='text-center'>Tu Libreria Aliada</h1>
+        </Col>
+        </Row>
+      </Container>
+    );
 }
+
+export default Login;
